92df1cd278670e15cf3ed29c2c918a68dc7f4274
Cetins <69481095+Cetins@users.noreply.github.com>
2025-10-08 21:56:04 +0100

    Components added to the test page

---
+++ frontend/app/test/page.tsx
@@ -0,0 +1,22 @@
+export default function TestPage() {
+    return (
+      <div>
+        <h1>Testing Components</h1>
+        <h2>Header 2</h2>
+        <h3>Header 3</h3>
+        <h4>Header 4</h4>
+        <h5>Header 5</h5>
+        <h6>Header 6</h6>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere, omnis! Ab distinctio amet laudantium facilis praesentium officiis eius laboriosam sapiente. Sapiente, fuga culpa ipsam voluptates odio voluptate blanditiis necessitatibus cum?</p>
+        <a href="/about" className="btn">Learn more</a>
+        <br/>
+        <button className="btn-fill">
+        Button Fill
+        </button>
+        <br/>
+        <button className="btn-outline">
+        Button Outline
+        </button>
+      </div>
+    );
+  }
