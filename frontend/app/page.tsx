cf40921348ecf2cddbbc5fce216c073056f62457
Cetins <69481095+Cetins@users.noreply.github.com>
2025-10-26 22:16:13 +0000

    vercel deployment error fixed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <section className={styles.hero}>
         <div className={styles.hero_text}>
           <h1>Turn Chaos into Clarity</h1>
-          <p>Running a small business can be overwhelming, but managing your sales, stock, and commissions doesn't have to be. Tilly brings everything together in one intuitive platform, giving you a clear view of your business at a glance. Track sales, monitor inventory, calculate commissions, and make informed decisions faster — all while saving time and reducing errors. Focus on growing your business and serving your customers, while Tilly takes care of the details.</p>
+          <p>Running a small business can be overwhelming, but managing your sales, stock, and commissions doesn&apos;t have to be. Tilly brings everything together in one intuitive platform, giving you a clear view of your business at a glance. Track sales, monitor inventory, calculate commissions, and make informed decisions faster — all while saving time and reducing errors. Focus on growing your business and serving your customers, while Tilly takes care of the details.</p>
           <div>
             <Link href="/signup" className={styles.btn_fill}>Join Us</Link>
           </div>
