eb789c23059b540d7ee299e54a55518792b8f63c
Cetins <69481095+Cetins@users.noreply.github.com>
2025-10-29 10:10:07 +0000

    Mobile sidebar completed in dashboard

---
+++ frontend/app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import Navigation from "../components/NavMenu/NavMenu";
+import Footer from "../components/Footer/Footer";
+export default function MarketingLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <header>
+        <Navigation />
+      </header>
+      <main>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+}
